fd458ed86ea8b9188bc343d1d93d64ba74e248b1
Ezequiel Natale <40499263+Eze-Nat@users.noreply.github.com>
2026-02-19 18:44:01 -0300

    fix: use process.env.PORT for production compatibility

--- src/config.js
+++ src/config.js
@@ -2,7 +2,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const PORT = 3000;
-export const DB_TYPE = process.env.DB_TYPE;
-export const DB_STORAGE = process.env.DB_STORAGE;
+export const PORT = process.env.PORT || 3000;
+
+export const DB_TYPE = process.env.DB_TYPE || "sqlite";
+export const DB_STORAGE = process.env.DB_STORAGE || "./database.sqlite";
+
 export const JWT_SECRET = process.env.JWT_SECRET;
